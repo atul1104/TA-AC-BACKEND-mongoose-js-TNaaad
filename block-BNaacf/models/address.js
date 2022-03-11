abfbf8474afde4c8704bdb391f016f127b745817
atul1104 <waghamareatul2@gmail.com>
2022-03-11 16:05:48 +0530

    assignment

--- block-BNaacf/models/address.js
+++ block-BNaacf/models/address.js
@@ -1,14 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var userSchema = new Schema({
-  name: String,
-  email: { type: String, lowercase: true },
-  age: { type: Number, default: 0 },
-  favourites: [String],
-  marks: [Number],
-});
-
 var ObjectId = mongoose.Schema.Types.ObjectId;
 var addressSchema = new Schema({
   village: String,
